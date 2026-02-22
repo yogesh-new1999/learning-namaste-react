15dc0d1fa81e4ae0a415312b5a42452ca8154cce
yogesh-new1999 <yogeshchaurasiya266@gmail.com>
2026-02-22 13:31:08 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = [
+  React.createElement("div", { id: "heading" }, [
+    React.createElement("h3", {}, "h1 react"),
+    React.createElement("h3", {}, "h3 react"),
+  ]),
+  React.createElement("h1", { id: "heading-2" }, "heading 2"),
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
